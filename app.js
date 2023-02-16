0cf6e16f715232a3b91161da8a4ced2a8810dc46
Guilherme Souza <89078191+guuisouza@users.noreply.github.com>
2023-02-16 14:03:26 +0000

    (16/02) Created Model & Migration

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+//Carrega as váriaveis de ambiente do arquivo .env para a aplicação.
+require ('dotenv').config()
+
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -7,6 +11,18 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+//Conection BD 
+const db = require('./models')
+
+try{
+    db.sequelize.authenticate()
+    console.log('SEQUELIZE: connection has been established sucessfully')
+}
+catch(error){
+    console.error('*SEQUELIZE: unable to connect to the database: ', error)
+    process.exit(1) //Encerrar o servidor com erro
+}
 
 app.use(logger('dev'));
 app.use(express.json());
