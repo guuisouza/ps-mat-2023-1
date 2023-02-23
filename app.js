f0bb55af5cdc2630a17692733d0e2a46bf167a98
Guilherme Souza <89078191+guuisouza@users.noreply.github.com>
2023-02-23 14:09:09 +0000

    (23/02) Primeiro controller e rota

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+/********************************/
+const users = require('./routes/users')
+app.use('users', users)
 
 module.exports = app;
---
+++ controllers/user.js
@@ -0,0 +1,27 @@
+// Importar o model correspondente ao controller
+
+const { User } = require('../models')
+
+const controller = {}
+
+/* 
+Métodos CRUD do controller
+create: cria um novo registro
+retrieve: lista(recupera) todos os registros
+retriaveOne: lista (recupera) apenas um registro
+update: atualiza um registro
+delete: exclui um registro
+*/
+
+controller.create = async (req, res) => {
+    try{
+        await User.create(req.body)
+        //HTTP 201: Created
+        res.status(201).end()
+    }
+    catch(error){
+        console.error(error)
+    }
+}
+
+module.exports = controller
