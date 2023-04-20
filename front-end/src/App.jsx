f4fb150e7f859566cdd5c3c8a707c55ea782159f
Guilherme Souza <89078191+guuisouza@users.noreply.github.com>
2023-04-20 14:09:03 +0000

    (20/04) Implementação do formulário de Login

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,18 +2,21 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
+import HeaderBar from './components/ui/HeaderBar'
+import Box from '@mui/material/Box'
 
 function App() {
 
   return (
-    <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/login" element={<Login />}/>
-        </Routes>
+        <HeaderBar />
+        <Box sx={{ m:'25px auto'}}>
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/login" element={<Login />}/>
+          </Routes>
+        </Box>
       </BrowserRouter>
-    </div>
   )
 }
 
--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,10 +1,63 @@
 import React from 'react'
+import Typography from '@mui/material/Typography'
+import Paper from '@mui/material/Paper'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
 
-export default function Login(){
+export default function Login() {
+    /* Criando 2 variaveis de estado para email e senha e associando aos campos */
+    const [email, setEmail] = React.useState('')
+    const [password, setPassword] = React.useState('')
+
+    function handleChange(event) {
+        if (event.target.name === 'email') setEmail(event.target.value)
+        else setPassword(event.target.value)
+    }
+
+    /* Submit que envia os dados dos campos ao back-end */
+    async function handleSubmit(event) {
+        event.preventDefault() //Impede o recarregamento da página
+        try {
+            let response = await fetch('http://localhost:3333/users/login', {
+                method: "POST",
+                body: JSON.stringify({ email, password }),
+                header: { "Content-type": "application/json; charset=UTF-8" }
+            })
+            console.log({ response })
+        }
+        catch (error) {
+            console.error(error)
+        }
+    }
+
+    /*Comunicação do backend com o frontend 
+    Metodos: xmlhttpRequest e o fetch
+
+    Axios facilita usar o xmlhttpRequest
+    Ky facilita usar o fetch
+
+
+    
+    */
     return (
         <>
-            <h1>Página de login</h1>
-            <p>Futura página de login</p>
+            <Typography variant="h3" component="h1" sx={{ textAlign: 'center' }}>
+                Autentique-se
+            </Typography>
+            <Paper sx={{
+                width: '512px',
+                maxWidth: '90%',
+                margin: '25px auto 0 auto',
+                p: '12px'
+            }}>
+                <Typography variant="h5" component="div">
+                    <form onSubmit={handleSubmit}>
+                        <TextField className='form-field' id="email" name="email" label="E-mail" variant="filled" fullWidth onChange={handleChange} value={email} />
+                        <TextField className='form-field' id="password" name="password" label="Senha" variant="filled" type="password" fullWidth onChange={handleChange} value={password} />
+                        <Button variant="contained" type="submit" color="secondary" fullWidth>Enviar</Button>
+                    </form>
+                </Typography>
+            </Paper>
         </>
     )
 }
